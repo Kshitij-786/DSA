ed5c52db4c73a9c7a17aba7746a09644cc167821
Kshitij-786 <varshneykshitij769@gmail.com>
2026-08-07 00:57:43 +0530

    update topic checklist

--- src/com/java/testing/test.java
+++ src/com/java/testing/test.java
@@ -5,5 +5,20 @@ public class test{
     static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
 
+      int n = sc.nextInt();
+      int rev = 0;
+      int og = n;
+      for(int i =1;i<=n;i++){
+          int digit = n%10;
+          rev = (rev *10) + digit;
+          n =n/10;
+      }
+        System.out.println(rev);
+      if(og == rev){
+          System.out.println("yes");
+      }
+      else {
+          System.out.println("no");
+      }
 }
 }
