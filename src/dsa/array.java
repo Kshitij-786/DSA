da8b306bf03746b5411c4f1df41de1541e73568c
Kshitij-786 <varshneykshitij769@gmail.com>
2026-08-09 01:00:06 +0530

    added Self DSA practice file

---
+++ src/dsa/array.java
@@ -0,0 +1,43 @@
+package dsa;
+//BASICS OF ARRAY
+import java.util.*;
+public class array {
+    static void main(String[] args) {
+
+        // print array elements
+//        int arr [] = {10,25,7,40,15};
+//        int i = 0;
+//        while(i<arr.length){
+//            System.out.println(arr[i]);
+//            i++;
+//        }
+
+        // sum of aaray all elements
+//        int arr [] = {10,20,30,40,50};
+//        int i = 0 , sum = 0;
+//        while(i<arr.length){
+//            sum += arr[i]; // sum = sum + arr[i];
+//            i++;
+//        }
+//        System.out.println(sum);
+
+        // find max element from array
+//        int arr [] = {10,45,23,89,12} , max = 0;
+//        for(int i = 0;i<arr.length;i++){
+//            if(arr[i]>max){
+//                max  = arr[i];
+//            }
+//        }
+//        System.out.println(max);
+
+        // fin min element of array
+//        int arr [] = {35,12,67,4,29};
+//        int min = arr[arr.length-1]; // can be arr[0]
+//        for(int i = 0;i< arr.length;i++){
+//            if(arr[i]<min){
+//                min = arr[i];
+//            }
+//        }
+//        System.out.println(min);
+    }
+}
