af20a9d0f5eab12894a68e7cc9bab96bbdb20397
Kshitij-786 <varshneykshitij769@gmail.com>
2026-08-10 00:03:46 +0530

    solved new array questions

--- src/dsa/array.java
+++ src/dsa/array.java
@@ -3,7 +3,7 @@ package dsa;
 import java.util.*;
 public class array {
     static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
         // print array elements
 //        int arr [] = {10,25,7,40,15};
 //        int i = 0;
@@ -39,5 +39,71 @@ public class array {
 //            }
 //        }
 //        System.out.println(min);
+
+        // count even number in array
+//        int arr[] = {10,15,22,7,8,13};
+//        int count = 0;
+//
+//        for(int i =0;i<arr.length;i++){
+//            if(arr[i]%2==0){
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+
+        // count positve numbers from array
+//        int arr[] = {-5,10,-2,7,0,15,-8};
+//        int posNum = 0;
+//        for (int i = 0;i< arr.length;i++){
+//            if(arr[i]>0){
+//                posNum++; // zero is not counted as positive number
+//            }
+//        }
+//        System.out.println(posNum);
+
+        // search element in array
+//        int arr[] = {10,25,7,40,15};
+//        int target  = sc.nextInt();
+//        boolean isFound = false;
+//        for (int i = 0;i< arr.length;i++){
+//            if(arr[i] == target){
+//                isFound = true;
+//            }
+//        }
+//        System.out.println(isFound);
+
+        // Count occurence in array
+//        int arr[] = {10,25,25,7,40,25,15};
+//        int target  = sc.nextInt();
+//        int count = 0;
+//        boolean isFound = false;
+//        for (int i = 0;i< arr.length;i++){
+//            if(arr[i] == target){
+//                isFound = true;
+//               count++;
+//            }
+//        }
+//        System.out.println(count);
+
+        // Reverse a Array
+//        int arr[] = {10,25,25,7,40,25,15};
+//        int rev[] = new int[arr.length];
+//        for(int i = 0;i<arr.length;i++){
+//            rev[i] = arr[arr.length-1-i];
+//         System.out.println(rev[i]); //valid but creates more space
+//        }
+
+        // Reverse a same array
+//        int arr[] = {10,20,30,40,50};
+//        int temp;
+//        for(int i = 0;i<arr.length/2;i++){ // divide 2 isliye kyuki index 0<->last se or phir 1 <-> sec last se vrna divide nhi karoge toh proper swap nhi ho paaega
+//            temp = arr[i];
+//            arr[i] = arr[arr.length-1 - i];
+//            arr[arr.length-1-i] = temp;
+//             // to get reversed array again start a loop
+//        }
+//        for(int i = 0;i< arr.length;i++){
+//            System.out.println(arr[i]);
+//        }
     }
 }
